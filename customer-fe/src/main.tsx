b223eac528a6d074a0eca6aa9fcf454e2c8635b3
Rafael H <ralph93.dev@gmail.com>
2026-01-17 19:11:30 -0500

    Add ClerkProvider to main.tsx and update dependencies in package.json

--- customer-fe/src/main.tsx
+++ customer-fe/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
 import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './hooks/useTheme'
@@ -7,14 +8,22 @@ import { apolloClient } from './lib/apollo'
 import './index.css'
 import App from './App'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ApolloProvider client={apolloClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ApolloProvider>
-    </ThemeProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ThemeProvider>
+        <ApolloProvider client={apolloClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ApolloProvider>
+      </ThemeProvider>
+    </ClerkProvider>
   </StrictMode>,
 )
